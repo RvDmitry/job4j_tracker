58433d64bd47c0d8fa6a1ad3222262efaa92564e
RvDmitry <rvdmitry@gmail.com>
2020-05-02 19:59:28 +0600

    5. Input и полиморфизм.[#266451]

---
+++ src/main/java/ru/job4j/tracker/StubInput.java
@@ -0,0 +1,47 @@
+package ru.job4j.tracker;
+
+/**
+ * Class StubInput
+ * Класс реализует интерфейс Input. Иммитирует взаимодействие пользователя с объектом Scanner.
+ * @author Dmitry Razumov
+ * @version 1
+ */
+public class StubInput implements Input {
+    /**
+     * Поле задает массив ответов пользователя.
+     */
+    private String[] answers;
+
+    /**
+     * Поле задает счетчик элементов массива.
+     */
+    private int position = 0;
+
+    /**
+     * Конструктор инициализирует массив ответов пользователя.
+     * @param answers Массив ответов пользователя
+     */
+    public StubInput(String[] answers) {
+        this.answers = answers;
+    }
+
+    /**
+     * Метод иммитирует ответы пользователя, возвращяя заранее переданный массив ответов поэлементно при вызове метода.
+     * @param question Вопрос на который нужно ответить пользователю
+     * @return Элементы массива ответов
+     */
+    @Override
+    public String askStr(String question) {
+        return answers[position++];
+    }
+
+    /**
+     * Метод иммитирует ответы пользователя, возвращяя заранее переданный массив ответов поэлементно при вызове метода.
+     * @param question Вопрос на который нужно ответить пользователю
+     * @return Элементы массива ответов
+     */
+    @Override
+    public int askInt(String question) {
+        return Integer.valueOf(askStr(question));
+    }
+}
