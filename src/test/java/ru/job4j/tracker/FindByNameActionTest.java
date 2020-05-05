f978a49446dbe2a6a16eddc20c3e79761410b85b
RvDmitry <rvdmitry@gmail.com>
2020-05-05 13:15:38 +0600

    9.2. Тесты вывода на консоль в StartUI[#266446]

---
+++ src/test/java/ru/job4j/tracker/FindByNameActionTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Class FindByNameActionTest
+ * @author Dmitry Razumov
+ * @version 1
+ */
+public class FindByNameActionTest {
+
+    @Test
+    public void whenCheckOutput() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        Tracker tracker = new Tracker();
+        Item item = new Item("Java Hot");
+        tracker.add(item);
+        FindByNameAction act = new FindByNameAction();
+        act.execute(new StubInput(new String[] {"Java Hot"}), tracker);
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("=== Find items by name ===")
+                .add(item.toString())
+                .add("Done.")
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
+    }
+}
